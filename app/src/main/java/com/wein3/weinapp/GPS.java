d6ae5c40857da706417ac884d6dd0c2bc027e649
Christian Hecktor <christian.hecktor@arcor.de>
2017-09-18 15:29:39 +0200

    Add support for built-in GPS sensor

--- app/src/main/java/com/wein3/weinapp/GPS.java
+++ app/src/main/java/com/wein3/weinapp/GPS.java
@@ -1,13 +1,99 @@
 package com.wein3.weinapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class GPS extends AppCompatActivity {
+public class GPS extends AppCompatActivity implements LocationListener {
+
+    private LocationManager locationManager;
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gps);
+        // initialize LocationManager instance
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // check permissions and enable location sensors accordingly
+        int gpsPermission = getBaseContext().checkCallingOrSelfPermission("android.permission.ACCESS_FINE_LOCATION");
+        int networkPermission = getBaseContext().checkCallingOrSelfPermission("android.permission.ACCESS_COARSE_LOCATION");
+        if (gpsPermission == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, this);
+        } else if (networkPermission == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, this);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 10000, 0, this);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // disable location sensors when activity is paused
+        locationManager.removeUpdates(this);
+    }
+
+    /**
+     * Called when the location has changed.
+     *
+     * @param location The new location, as a Location object.
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+    }
+
+    /**
+     * Called when the provider status changes.
+     *
+     * @param provider the name of the location provider associated with this
+     *                 update.
+     * @param status   {@link LocationProvider#OUT_OF_SERVICE} if the
+     *                 provider is out of service, and this is not expected to change in the
+     *                 near future; {@link LocationProvider#TEMPORARILY_UNAVAILABLE} if
+     *                 the provider is temporarily unavailable but is expected to be available
+     *                 shortly; and {@link LocationProvider#AVAILABLE} if the
+     *                 provider is currently available.
+     * @param extras   an optional Bundle which will contain provider specific
+     *                 status variables.
+     */
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    /**
+     * Called when the provider is enabled by the user.
+     *
+     * @param provider the name of the location provider associated with this
+     *                 update.
+     */
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    /**
+     * Called when the provider is disabled by the user.
+     *
+     * @param provider the name of the location provider associated with this
+     *                 update.
+     */
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
--- app/src/main/java/com/wein3/weinapp/MainActivity.java
+++ app/src/main/java/com/wein3/weinapp/MainActivity.java
@@ -1,19 +1,10 @@
 package com.wein3.weinapp;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonMap = (Button) findViewById(R.id.button_map);
-
         buttonMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         buttonDatabase = (Button) findViewById(R.id.button_database);
-
         buttonDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
         buttonGPS = (Button) findViewById(R.id.button_gps);
-
         buttonGPS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
