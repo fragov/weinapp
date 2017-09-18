a17f7d3e022ed809d43861e49e1ce4d4b3995e5f
alex <alkehl@students.uni-mainz.de>
2017-09-18 12:09:04 +0200

    Added dependencies

--- app/src/main/java/com/wein3/weinapp/MainActivity.java
+++ app/src/main/java/com/wein3/weinapp/MainActivity.java
@@ -3,11 +3,14 @@ package com.wein3.weinapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.Mapbox;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.access_token));
         setContentView(R.layout.activity_main);
     }
 }
