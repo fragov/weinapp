17c02d5edb0894b9c553464ef19694ecca3ca0cb
Christian Hecktor <christian.hecktor@arcor.de>
2017-09-19 16:26:20 +0200

    Attention: Experimental version of GPS tracker

--- app/src/main/java/com/wein3/weinapp/Map.java
+++ app/src/main/java/com/wein3/weinapp/Map.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.PolylineOptions;
@@ -19,22 +18,15 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
-import java.util.List;
-
 public class Map extends AppCompatActivity implements LocationListener {
 
-    public static final String TAG = Map.class.getSimpleName();
+    private LocationManager locationManager;
 
     private MapView mapView;
-    private MapboxMap map;
-    private LocationManager locationManager;
+    private PolylineOptions options;
+
     private double latitude;
     private double longitude;
-    boolean hasNew = false;
-    private List<LatLng> Points;
-    private PolylineOptions options = new PolylineOptions();
-    private LocationManager manager;
-    private LocationListener listener;
 
 
     @Override
@@ -42,18 +34,14 @@ public class Map extends AppCompatActivity implements LocationListener {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, getString(R.string.access_token));
         setContentView(R.layout.activity_map);
-
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // show user dialog
             if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
             }
         }
-
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // show user dialog
             if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
             }
@@ -61,30 +49,34 @@ public class Map extends AppCompatActivity implements LocationListener {
 
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.onCreate(savedInstanceState);
+
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
-                map = mapboxMap;
+                options = new PolylineOptions();
+                Location location;
+                int gpsPermission = getBaseContext().checkCallingOrSelfPermission("android.permission.ACCESS_FINE_LOCATION");
+                int networkPermission = getBaseContext().checkCallingOrSelfPermission("android.permission.ACCESS_COARSE_LOCATION");
+                if (gpsPermission == PackageManager.PERMISSION_GRANTED) {
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                } else if (networkPermission == PackageManager.PERMISSION_GRANTED) {
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                } else {
+                    location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+                }
+                options.add(new LatLng(location.getLatitude(), location.getLongitude()));
+                mapboxMap.addPolyline(options);
             }
         });
 
-        CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(new LatLng(41.327752, 19.818666)) // Sets the center of the map to the specified location
-                .zoom(13)                            // Sets the zoom level
-                .build();
-    }
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(41.327752, 41.327752)).zoom(13).build();
 
+    }
 
     @Override
     public void onStart() {
         super.onStart();
         mapView.onStart();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
         int gpsPermission = getBaseContext().checkCallingOrSelfPermission("android.permission.ACCESS_FINE_LOCATION");
         int networkPermission = getBaseContext().checkCallingOrSelfPermission("android.permission.ACCESS_COARSE_LOCATION");
         if (gpsPermission == PackageManager.PERMISSION_GRANTED) {
@@ -97,16 +89,22 @@ public class Map extends AppCompatActivity implements LocationListener {
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
     public void onPause() {
         super.onPause();
         mapView.onPause();
-        locationManager.removeUpdates(this);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         mapView.onStop();
+        locationManager.removeUpdates(this);
     }
 
     @Override
@@ -130,13 +128,11 @@ public class Map extends AppCompatActivity implements LocationListener {
 
     @Override
     public void onLocationChanged(Location location) {
-
         if (location != null) {
             latitude = location.getLatitude();
             longitude = location.getLongitude();
         }
-        map.addPolyline(options.add(new LatLng(latitude, longitude)));
-        Toast.makeText(getApplicationContext(), latitude + " " + longitude, Toast.LENGTH_LONG).show();
+        options.add(new LatLng(latitude, longitude));
     }
 
     @Override
