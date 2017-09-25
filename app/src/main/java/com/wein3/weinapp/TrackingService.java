dec2b9604ab25c4d9b486a2f5af3562a6d8ebacd
alex <alkehl@students.uni-mainz.de>
2017-09-25 12:31:35 +0200

    Added record notification

--- app/src/main/java/com/wein3/weinapp/TrackingService.java
+++ app/src/main/java/com/wein3/weinapp/TrackingService.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
+import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
@@ -24,7 +25,7 @@ public class TrackingService extends Service implements MapboxMap.OnMyLocationCh
     private MapboxMap mapboxMap;
     private StringBuilder sb;
     private NotificationManager mNM;
-    //private int NOTIFICATION = R.string.local_service_started;
+    private int NOTIFICATION = R.string.local_service_started;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -33,11 +34,12 @@ public class TrackingService extends Service implements MapboxMap.OnMyLocationCh
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        //get the Manager for the notification
         mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-
         // Display a notification about us starting.  We put an icon in the status bar.
         showRecordingNotification();
-
+        //gets an instance of Mapbox with our token
+        Mapbox.getInstance(this, getString(R.string.access_token));
         //get the Mapview from the xml Layout file
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         View layout = inflater.inflate(R.layout.content_map, null);
@@ -60,7 +62,7 @@ public class TrackingService extends Service implements MapboxMap.OnMyLocationCh
         super.onDestroy();
         //this service sends the whole String with all coordinates to the Map activity
         sendMessageToActivity(sb.toString());
-       // mNM.cancel(NOTIFICATION);
+        mNM.cancel(NOTIFICATION);
     }
 
 
@@ -72,7 +74,21 @@ public class TrackingService extends Service implements MapboxMap.OnMyLocationCh
     }
 
     private void showRecordingNotification(){
+        // The PendingIntent to launch our activity if the user selects this notification
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, Map.class), 0);
 
+        // Set the info for the views that show in the notification panel.
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.ic_record)// the status icon
+                .setTicker(getText(R.string.local_service_started))  // the status text
+                .setWhen(System.currentTimeMillis())  // the time stamp
+                .setContentTitle(getText(R.string.local_service_started))  // the label of the entry
+                .setContentIntent(contentIntent)  // The intent to send when the entry is clicked
+                .build();
+
+        // Send the notification.
+        mNM.notify(NOTIFICATION, notification);
     }
 
     private void sendMessageToActivity(String message) {
